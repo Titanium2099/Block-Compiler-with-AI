a0eb89213533620bca8cb450b023e447a66d3190
Titanium2099 <yasgh@protonmail.com>
2025-02-13 20:11:11 -0800

    clear chat button

--- src/addons/addons/ai-integration/userscript.js
+++ src/addons/addons/ai-integration/userscript.js
@@ -27,6 +27,8 @@ document.AI_INTEGRATION = {
   canUse: true,
 };
 
+const originalState = document.AI_INTEGRATION;
+
 document.addEventListener("mousemove", (event) => {
   document.AI_INTEGRATION.X_COORDINATE = event.clientX;
   document.AI_INTEGRATION.Y_COORDINATE = event.clientY;
@@ -200,7 +202,11 @@ function createBasePopup(fileAttached = false, fileAttachedText = "Unknown - Ent
   div.style.left = `${newLeft}px`;
   div.style.top = `${newTop}px`;
   div.innerHTML = `
-  <div style="padding: 5px;display: flex;"><svg fill="none" viewBox="0 0 24 24" xmlns="http://" www.w3.org="" 2000="" svg="" class="size-6" stroke="currentColor" stroke-width="1.5" style="height:19px;color:#fff;margin-top:auto;margin-bottom:auto;cursor:pointer;margin-left: auto;" id="closePopup">
+  <div style="padding: 5px;display: flex;">
+  <svg id="clearChat" style="height:19px;color:#fff;margin-top:auto;margin-bottom:auto;cursor:pointer;margin-left: auto;" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+  <path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
+</svg>
+  <svg fill="none" viewBox="0 0 24 24" xmlns="http://" www.w3.org="" 2000="" svg="" class="size-6" stroke="currentColor" stroke-width="1.5" style="height:19px;color:#fff;margin-top:auto;margin-bottom:auto;cursor:pointer;margin-left: 10px;" id="closePopup">
                 <path d="M6 18 18 6M6 6l12 12" stroke-linecap="round" stroke-linejoin="round"></path>
             </svg>
   </div>
@@ -339,7 +345,15 @@ function popupFunctionality() {
     document.querySelector('.container').style.zIndex = -999999;
     document.AI_INTEGRATION.popupOpen = false;
   });
-
+  document.getElementById('clearChat').addEventListener('click', () => {
+    //prompt user if they are sure
+    if (confirm("Are you sure you want to clear the chat?")) {
+      while(document.getElementById('chat_content').children.length > 1){
+        document.getElementById('chat_content').children[1].remove();
+      }
+      document.AI_INTEGRATION = originalState;
+    }
+  });
   document.getElementById('submitChat').addEventListener('click', () => {
     internal();
   });
